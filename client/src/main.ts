a0609e40ed0d8e81aff06d9d9f54d079945590fe
shamsnahidk <102136391+dreamerloading5@users.noreply.github.com>
2026-04-30 13:45:22 -0500

    Add simple town layout with collisions

--- client/src/main.ts
+++ client/src/main.ts
@@ -12,6 +12,7 @@ type ArcadeRectangle = Phaser.GameObjects.Rectangle & {
 class WorldScene extends Phaser.Scene {
   private player!: ArcadeRectangle;
   private npc!: ArcadeRectangle;
+  private obstacles!: Phaser.Physics.Arcade.StaticGroup;
 
   private cursors!: Phaser.Types.Input.Keyboard.CursorKeys;
   private keys!: {
@@ -20,11 +21,14 @@ class WorldScene extends Phaser.Scene {
     S: Phaser.Input.Keyboard.Key;
     D: Phaser.Input.Keyboard.Key;
     E: Phaser.Input.Keyboard.Key;
+    ESC: Phaser.Input.Keyboard.Key;
   };
 
   private dialogueBox!: Phaser.GameObjects.Rectangle;
   private dialogueText!: Phaser.GameObjects.Text;
   private hintText!: Phaser.GameObjects.Text;
+
+  private isDialogueOpen = false;
 
   constructor() {
     super("WorldScene");
@@ -32,20 +36,29 @@ class WorldScene extends Phaser.Scene {
 
   create() {
     this.createWorld();
+    this.createObstacles();
     this.createPlayer();
     this.createNPC();
     this.createUI();
     this.createControls();
 
+    this.physics.add.collider(this.player, this.obstacles);
     this.physics.add.collider(this.player, this.npc);
   }
 
   update() {
-    this.handleMovement();
+    if (!this.isDialogueOpen) {
+      this.handleMovement();
+    } else {
+      this.player.body.setVelocity(0);
+    }
+
     this.handleInteraction();
+    this.handleDialogueClose();
   }
 
   private createWorld() {
+    // Grass background
     this.add.rectangle(
       GAME_WIDTH / 2,
       GAME_HEIGHT / 2,
@@ -54,23 +67,102 @@ class WorldScene extends Phaser.Scene {
       0x8fd694
     );
 
-    this.add.text(24, 20, "MemoryTown - Day 1 Prototype", {
+    // Main road
+    this.add.rectangle(GAME_WIDTH / 2, 280, GAME_WIDTH, 90, 0xd9c7a3);
+
+    // Vertical road
+    this.add.rectangle(460, GAME_HEIGHT / 2, 90, GAME_HEIGHT, 0xd9c7a3);
+
+    // Road intersection
+    this.add.rectangle(460, 280, 110, 110, 0xcbb68f);
+
+    this.add.text(24, 20, "MemoryTown - Day 2 Prototype", {
       fontSize: "20px",
       color: "#102a43",
       fontFamily: "Arial",
     });
 
-    this.add.text(24, 48, "Move: WASD / Arrow Keys | Interact: E", {
+    this.add.text(24, 48, "Move: WASD / Arrow Keys | Talk: E | Close: ESC", {
+      fontSize: "15px",
+      color: "#102a43",
+      fontFamily: "Arial",
+    });
+
+    // Location labels
+    this.add.text(115, 112, "Coffee Shop", {
+      fontSize: "15px",
+      color: "#102a43",
+      fontFamily: "Arial",
+    });
+
+    this.add.text(680, 112, "Library", {
+      fontSize: "15px",
+      color: "#102a43",
+      fontFamily: "Arial",
+    });
+
+    this.add.text(120, 420, "Apartments", {
+      fontSize: "15px",
+      color: "#102a43",
+      fontFamily: "Arial",
+    });
+
+    this.add.text(680, 420, "Park", {
       fontSize: "15px",
       color: "#102a43",
       fontFamily: "Arial",
     });
   }
 
+  private createObstacles() {
+    this.obstacles = this.physics.add.staticGroup();
+
+    this.createBuilding(170, 155, 180, 110, 0xb56576);
+    this.createBuilding(735, 155, 190, 120, 0x6d597a);
+    this.createBuilding(175, 445, 190, 100, 0x355070);
+
+    // Park trees
+    this.createTree(670, 405);
+    this.createTree(760, 410);
+    this.createTree(710, 470);
+    this.createTree(830, 455);
+
+    // Small fountain
+    const fountain = this.add.rectangle(735, 455, 70, 45, 0x4ea8de) as ArcadeRectangle;
+    this.physics.add.existing(fountain, true);
+    this.obstacles.add(fountain);
+  }
+
+  private createBuilding(
+    x: number,
+    y: number,
+    width: number,
+    height: number,
+    color: number
+  ) {
+    const building = this.add.rectangle(x, y, width, height, color) as ArcadeRectangle;
+    this.physics.add.existing(building, true);
+    this.obstacles.add(building);
+
+    // Roof highlight
+    this.add.rectangle(x, y - height / 2 + 12, width, 24, 0x2f2f2f);
+  }
+
+  private createTree(x: number, y: number) {
+    const trunk = this.add.rectangle(x, y + 16, 16, 28, 0x8b5a2b);
+    const leaves = this.add.circle(x, y, 28, 0x2f855a) as unknown as ArcadeRectangle;
+
+    this.physics.add.existing(leaves, true);
+    this.obstacles.add(leaves);
+
+    trunk.setDepth(1);
+    leaves.setDepth(2);
+  }
+
   private createPlayer() {
     this.player = this.add.rectangle(
-      200,
-      260,
+      460,
+      280,
       34,
       46,
       0x2f80ed
@@ -78,20 +170,22 @@ class WorldScene extends Phaser.Scene {
 
     this.physics.add.existing(this.player);
     this.player.body.setCollideWorldBounds(true);
+    this.player.setDepth(10);
   }
 
   private createNPC() {
     this.npc = this.add.rectangle(
-      620,
-      260,
+      300,
+      275,
       40,
       54,
       0xf2c94c
     ) as ArcadeRectangle;
 
     this.physics.add.existing(this.npc, true);
+    this.npc.setDepth(10);
 
-    this.add.text(585, 200, "Maya", {
+    this.add.text(270, 220, "Maya", {
       fontSize: "16px",
       color: "#102a43",
       fontFamily: "Arial",
@@ -111,6 +205,7 @@ class WorldScene extends Phaser.Scene {
     });
 
     this.hintText.setVisible(false);
+    this.hintText.setDepth(100);
 
     this.dialogueBox = this.add.rectangle(
       GAME_WIDTH / 2,
@@ -118,7 +213,7 @@ class WorldScene extends Phaser.Scene {
       GAME_WIDTH - 80,
       110,
       0x111111,
-      0.9
+      0.92
     );
 
     this.dialogueText = this.add.text(70, GAME_HEIGHT - 120, "", {
@@ -132,6 +227,9 @@ class WorldScene extends Phaser.Scene {
 
     this.dialogueBox.setVisible(false);
     this.dialogueText.setVisible(false);
+
+    this.dialogueBox.setDepth(100);
+    this.dialogueText.setDepth(101);
   }
 
   private createControls() {
@@ -143,12 +241,14 @@ class WorldScene extends Phaser.Scene {
       S: Phaser.Input.Keyboard.KeyCodes.S,
       D: Phaser.Input.Keyboard.KeyCodes.D,
       E: Phaser.Input.Keyboard.KeyCodes.E,
+      ESC: Phaser.Input.Keyboard.KeyCodes.ESC,
     }) as {
       W: Phaser.Input.Keyboard.Key;
       A: Phaser.Input.Keyboard.Key;
       S: Phaser.Input.Keyboard.Key;
       D: Phaser.Input.Keyboard.Key;
       E: Phaser.Input.Keyboard.Key;
+      ESC: Phaser.Input.Keyboard.Key;
     };
   }
 
@@ -180,16 +280,37 @@ class WorldScene extends Phaser.Scene {
 
     const isNearNPC = distance < 90;
 
-    this.hintText.setVisible(isNearNPC);
+    this.hintText.setVisible(isNearNPC && !this.isDialogueOpen);
     this.hintText.setPosition(this.npc.x - 50, this.npc.y - 85);
 
-    if (isNearNPC && Phaser.Input.Keyboard.JustDown(this.keys.E)) {
-      this.dialogueBox.setVisible(true);
-      this.dialogueText.setVisible(true);
-
-      this.dialogueText.setText(
+    if (
+      isNearNPC &&
+      !this.isDialogueOpen &&
+      Phaser.Input.Keyboard.JustDown(this.keys.E)
+    ) {
+      this.openDialogue(
         "Maya: Hey, you must be new here. I run the coffee shop in MemoryTown. People talk a lot around here, so listen carefully."
       );
+    }
+  }
+
+  private openDialogue(message: string) {
+    this.isDialogueOpen = true;
+    this.dialogueBox.setVisible(true);
+    this.dialogueText.setVisible(true);
+    this.dialogueText.setText(message);
+  }
+
+  private closeDialogue() {
+    this.isDialogueOpen = false;
+    this.dialogueBox.setVisible(false);
+    this.dialogueText.setVisible(false);
+    this.dialogueText.setText("");
+  }
+
+  private handleDialogueClose() {
+    if (this.isDialogueOpen && Phaser.Input.Keyboard.JustDown(this.keys.ESC)) {
+      this.closeDialogue();
     }
   }
 }
